5bca375d2f3e642e937bd3c2533bef222419ccc5
sakib <mohammadsakib7679@gmail.com>
2025-08-31 22:45:43 +0600

    bellmanFord function for clarity in bellman-ford file

--- 4_graph_algorithm/Algorithm/Bellmen-ford/bellman-ford.cpp
+++ 4_graph_algorithm/Algorithm/Bellmen-ford/bellman-ford.cpp
@@ -1,49 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int dis[1005];
-
-class Edge{
-    public:
-        int a;
-        int b;
-        int c;
-        Edge(int a, int b, int c){
-            this->a = a;
-            this->b = b;
-            this->c = c;
-        }
+class Edge {
+public:
+    int a;
+    int b;
+    int c;
+    Edge(int a, int b, int c) {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
 };
 
-int main(){
-    int v, e; cin >> v >> e;
-    vector<Edge> adj_list;
-    while (e--)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        adj_list.push_back(Edge(a, b, c));
-    }
+int v, e; 
+int dis[1005];
+vector<Edge> adj_list;
 
-    for(int i = 0; i<v; i++){
-        dis[i] = INT_MAX;
-    }
-    dis[0] = 0;
-    for(int i = 0; i<v-1; i++){
-        for(auto edge : adj_list){
+void bellmanFord() {
+    // Relax v-1 times
+    for (int i = 0; i < v - 1; i++) {
+        for (auto edge : adj_list) {
             int a = edge.a;
             int b = edge.b;
             int c = edge.c;
 
-            if(dis[a] != INT_MAX && dis[a] + c < dis[b]){
+            if (dis[a] != INT_MAX && dis[a] + c < dis[b]) {
                 dis[b] = dis[a] + c;
             }
         }
     }
 
-    for(int i = 0; i<v; i++){
+    // Print results
+    for (int i = 0; i < v; i++) {
         cout << i << " => " << dis[i] << endl;
     }
-    
+}
+
+int main() {
+    cin >> v >> e;
+
+    while (e--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_list.push_back(Edge(a, b, c));
+    }
+
+    for (int i = 0; i < v; i++) {
+        dis[i] = INT_MAX;
+    }
+    dis[0] = 0;
+
+    bellmanFord();
+
     return 0;
 }
