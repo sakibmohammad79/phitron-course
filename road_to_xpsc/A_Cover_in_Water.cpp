96d26303eec67280fc5f2874275a5939e27df1eb
sakib <mohammadsakib7679@gmail.com>
2025-09-20 20:03:31 +0600

    solve cover in water codeforces problem

---
+++ road_to_xpsc/A_Cover_in_Water.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        string s; cin >> s;
+        int consicutive = 0;
+        int empty = 0;
+        for(char c : s){
+            if(c == '.'){
+                empty++;
+                consicutive++;
+                if(consicutive == 3){
+                    break;
+                }
+            }
+            else{
+                consicutive = 0;
+            }
+        }
+        if(consicutive == 3){
+            cout << 2 << endl;
+        }
+        else{
+            cout << empty << endl;
+        }
+    }
+    
+    return 0;
+}
