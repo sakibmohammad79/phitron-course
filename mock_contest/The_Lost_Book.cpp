8262ab468a5dd31338756a20d7eeaf96edf0ce2b
sakib <mohammadsakib7679@gmail.com>
2025-09-24 21:25:56 +0600

    solve book found problem

---
+++ mock_contest/The_Lost_Book.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int target; 
+    cin >> target;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] == target){
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+    return 0;
+}
