b3cdf77da9e65269cca8303b98b1be32c9de5385
sakib <mohammadsakib7679@gmail.com>
2025-10-01 23:43:44 +0600

    complete knapsack variation module

---
+++ 5_dynamic_programming/17_knapsack_variation/1_subset_sum.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int val[1005];
+int dp[1005][1005];
+bool subset_sum(int i, int sum){
+    //base case
+    if(i<0){
+        if(sum == 0)
+            return true;
+        else   
+            return false;
+    }
+    //condition
+    if(dp[i][sum] != -1){
+        return dp[i][sum];
+    }
+    if(val[i] <= sum){
+        bool option1 = subset_sum(i-1, sum-val[i]);
+        bool option2 = subset_sum(i-1, sum);
+        dp[i][sum] = option1 || option2;
+        return dp[i][sum];
+    }
+    else{
+        dp[i][sum] = subset_sum(i-1, sum);
+        return dp[i][sum];
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i<n; i++){
+        cin >> val[i];
+    }
+    int sum; cin >> sum;
+
+    for(int i = 0; i<=n; i++){
+        for(int j = 0; j<=sum; j++){
+            dp[i][j] = -1;
+        }
+    }
+
+    if(subset_sum(n-1, sum))
+        cout << "yes\n";
+    else    
+        cout << "no\n";
+    return 0;
+}
